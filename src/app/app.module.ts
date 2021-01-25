963fce547023a1a23dd9c33bc3752f3557d959cb
Liam <liam@verifaction.co.za>
2021-01-25 17:35:41 +0200

    Added design for navbar and project cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ProjectComponent } from './components/project/project.component';
+import { SectionComponent } from './components/section/section.component';
+import { TaskComponent } from './components/task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    NavBarComponent,
+    ProjectComponent,
+    SectionComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
